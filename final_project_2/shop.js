7bfcf3d0e1c07fe40dd9f8974ffa4da8c5404012
vabrigadina <144808992+vabrigadina@users.noreply.github.com>
2023-12-06 15:24:10 +0300

    Update shop.js

--- final_project_2/shop.js
+++ final_project_2/shop.js
@@ -1,5 +1,5 @@
 async function getResponce() {
-    let responce = await fetch("./shop.json")
+    let responce = await fetch("final_project_2/shop.json")
 
     let content = await responce.text()
     console.log(content)
